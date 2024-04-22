f69fb201106fc119f6d46c7f2c220920810481d3
brapcan <rapcanborna@gmail.com>
2024-04-22 15:14:35 +0200

    Small updates to readme and export.py

--- export.py
+++ export.py
@@ -5,18 +5,21 @@ from utils.create_df import process_txt_files
 home_dir = os.path.expanduser("~")
 
 # Navigate to the Desktop folder and the export folder
-dir_path = os.path.join(home_dir, "Desktop", "export")
+dir_path = os.path.join(home_dir, "Desktop", "t_export", "stand_samp_dex_pool")
+
+print(dir_path)
 
 # Call the function to process text files and get the dictionary of DataFrames
 dfs_dict = process_txt_files(dir_path, export_area=True)
 
-# Loop through the dictionary of DataFrames
-for file_path, df in dfs_dict.items():
-    # Extract the filename from the file path
-    filename = os.path.basename(file_path)
+print(dfs_dict)
+# # Loop through the dictionary of DataFrames
+# for file_path, df in dfs_dict.items():
+#     # Extract the filename from the file path
+#     filename = os.path.basename(file_path)
 
-    # Define the Excel filename
-    excel_filename = os.path.splitext(filename)[0] + ".xlsx"
+#     # Define the Excel filename
+#     excel_filename = os.path.splitext(filename)[0] + ".xlsx"
 
-    # Export the DataFrame to an Excel file
-    df.to_excel(os.path.join(dir_path, excel_filename), index=False)
+#     # Export the DataFrame to an Excel file
+#     df.to_excel(os.path.join(dir_path, excel_filename), index=False)
--- utils/create_df.py
+++ utils/create_df.py
@@ -23,86 +23,91 @@ def process_txt_files(dir_path, export_area=False):
                         f"Error: Required columns not found in {filename}. Skipping..."
                     )
                     continue
+                else:
+                    # Get the index of each required column
+                    sample_name_idx = header.index("SampleName")
+                    percent_area_idx = header.index("% Area")
 
-                # Get the index of each required column
-                sample_name_idx = header.index("SampleName")
-                percent_area_idx = header.index("% Area")
-
-                # Check if the "Area" column is present and export_area is True
-                area_idx = (
-                    header.index("Area") if "Area" in header and export_area else None
-                )
-
-                # Create a dictionary to store the data for each sample name
-                sample_data = {}
-
-                # Extract data from each line and group it by sample name
-                for line in lines[1:]:
-                    cols = line.strip().split("\t")
-
-                    sample_name = cols[sample_name_idx]
-                    percent_area = cols[percent_area_idx]
-
-                    if percent_area == "%Area":
-                        continue
-
-                    if sample_name not in sample_data:
-                        sample_data[sample_name] = {
-                            "percent_areas": [],
-                            "areas": [] if area_idx is not None else None,
-                        }
-
-                    # Convert percent_area to float and append it to the list
-                    if "," in percent_area:
-                        percent_area = percent_area.replace(",", ".")
-                    try:
-                        percent_area_float = float(percent_area)
-                    except ValueError:
-                        continue
-                    sample_data[sample_name]["percent_areas"].append(percent_area_float)
-
-                    # Process the "Area" information if available
-                    if area_idx is not None:
-                        area = cols[area_idx]
-                        try:
-                            area_int = float(area)
-                        except ValueError:
-                            area_int = ""
-                        sample_data[sample_name]["areas"].append(area_int)
-
-                # Create a list to store the rows for the Excel file
-                rows = []
-
-                # Add a row for each sample name
-                for sample_name, data in sample_data.items():
-                    percent_areas = [
-                        round(float(val), 5) for val in data["percent_areas"]
-                    ]
-                    areas = (
-                        [val for val in data["areas"]] if area_idx is not None else []
+                    # Check if the "Area" column is present and export_area is True
+                    area_idx = (
+                        header.index("Area")
+                        if "Area" in header and export_area
+                        else None
                     )
 
-                    # Check if the percent_areas list is not empty before adding the row
-                    if percent_areas:
-                        row = [sample_name] + ["% Area"] + percent_areas
-                        rows.append(row)
+                    # Create a dictionary to store the data for each sample name
+                    sample_data = {}
+
+                    # Extract data from each line and group it by sample name
+                    for line in lines[1:]:
+                        cols = line.strip().split("\t")
+
+                        sample_name = cols[sample_name_idx]
+                        percent_area = cols[percent_area_idx]
+
+                        if percent_area == "%Area":
+                            continue
+
+                        if sample_name not in sample_data:
+                            sample_data[sample_name] = {
+                                "percent_areas": [],
+                                "areas": [] if area_idx is not None else None,
+                            }
+
+                        # Convert percent_area to float and append it to the list
+                        if "," in percent_area:
+                            percent_area = percent_area.replace(",", ".")
+                        try:
+                            percent_area_float = float(percent_area)
+                        except ValueError:
+                            continue
+                        sample_data[sample_name]["percent_areas"].append(
+                            percent_area_float
+                        )
+
+                        # Process the "Area" information if available
                         if area_idx is not None:
-                            row = [sample_name] + ["Area"] + areas
+                            area = cols[area_idx]
+                            try:
+                                area_int = float(area)
+                            except ValueError:
+                                area_int = ""
+                            sample_data[sample_name]["areas"].append(area_int)
+
+                    # Create a list to store the rows for the Excel file
+                    rows = []
+
+                    # Add a row for each sample name
+                    for sample_name, data in sample_data.items():
+                        percent_areas = [
+                            round(float(val), 5) for val in data["percent_areas"]
+                        ]
+                        areas = (
+                            [val for val in data["areas"]]
+                            if area_idx is not None
+                            else []
+                        )
+
+                        # Check if the percent_areas list is not empty before adding the row
+                        if percent_areas:
+                            row = [sample_name] + ["% Area"] + percent_areas
                             rows.append(row)
+                            if area_idx is not None:
+                                row = [sample_name] + ["Area"] + areas
+                                rows.append(row)
 
-                # Sort the rows by the first column (sample name)
-                rows = sorted(rows, key=lambda row: row[0])
+                    # Sort the rows by the first column (sample name)
+                    rows = sorted(rows, key=lambda row: row[0])
 
-                # Create a dataframe from the rows
-                df = pd.DataFrame(rows)
+                    # Create a dataframe from the rows
+                    df = pd.DataFrame(rows)
 
-                # Create a list of header labels for the dataframe
-                header = ["Sample Name", "Area type"] + [
-                    f"GP{i+1}" for i in range(df.shape[1] - 2)
-                ]
-                df.columns = header
+                    # Create a list of header labels for the dataframe
+                    header = ["Sample Name", "Area type"] + [
+                        f"GP{i+1}" for i in range(df.shape[1] - 2)
+                    ]
+                    df.columns = header
 
-                # Add the DataFrame to the dictionary with the file path as the key
-                dfs[file_path] = df
-
+                    # Add the DataFrame to the dictionary with the file path as the key
+                    dfs[file_path] = df
     return dfs
